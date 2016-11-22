67859346178f49046edf5ce4f6cce9039c9ffc8c
youhandan <youhandan@163.com>
2016-11-22 19:21:10 +0800

    Init project

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!!!<br />
+                欢迎来到菜鸟教程学习！！！
+            </div>
+        );
+    }
+}
+
+export default App;

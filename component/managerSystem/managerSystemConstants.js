29429b16c72f5c5c5d59ea25daaf9e384df3ce5b
youhandan <youhandan@163.com>
2016-11-27 22:54:36 +0800

    Change managerSystemConstants

--- component/managerSystem/managerSystemConstants.js
+++ component/managerSystem/managerSystemConstants.js
@@ -2,13 +2,13 @@
  * Created by youhandan on 2016/11/25.
  */
 export const sexes = [
-    { text: '男', value: 'male' },
-    { text: '女', value: 'female' },
+    { text: '男', value: '男' },
+    { text: '女', value: '女' },
 ]
 
 export const roles = [
-    { text: '主任', value: 'master' },
-    { text: '学生', value: 'student' },
-    { text: '老师', value: 'teacher' },
-    { text: '实习', value: 'intern' },
+    { text: '主任', value: '主任' },
+    { text: '学生', value: '学生' },
+    { text: '老师', value: '老师' },
+    { text: '实习', value: '实习' },
 ]
